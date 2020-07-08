5fcaaa1d2f5db756c59411ecd073e538ae5d76d1
Christian Riese <christian.riese@atos.net>
2020-07-08 16:17:23 +0200

    fixes

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@ apt-get -y install nodejs
 apt-get -y install libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npm install -g cypress --unsafe-perm --silent
 
-
 mkdir /.cache
 mkdir /.cache/Cypress
 mv -v /root/.cache/Cypress/* /.cache/Cypress
+export CYPRESS_CACHE_FOLDER='/.cache/Cypress'
+cypress verify
